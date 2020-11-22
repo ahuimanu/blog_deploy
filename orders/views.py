730f8f69d22e5812d3d481c4ad3eae474bfd9f50
Jeffry Babb <ahuimanu@gmail.com>
2020-11-22 14:19:30 -0600

    version without weasyprint

--- orders/views.py
+++ orders/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import redirect, render, get_object_or_404
 from django.template.loader import render_to_string
 from django.urls import reverse
 
-import weasyprint
+# import weasyprint
 
 from .email import order_created
 from .forms import OrderCreateForm
@@ -49,15 +49,15 @@ def admin_order_detail(request, order_id):
                   {'order': order})
 
 
-@staff_member_required
-def admin_order_pdf(request, order_id):
-    order = get_object_or_404(Order, id=order_id)
-    html = render_to_string('orders/order/pdf.html',
-                            {'order': order})
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
-    weasyprint.HTML(string=html).write_pdf(response,
-        stylesheets=[weasyprint.CSS(
-            settings.STATIC_ROOT + 'css/pdf.css')])
-    return response
+# @staff_member_required
+# def admin_order_pdf(request, order_id):
+#     order = get_object_or_404(Order, id=order_id)
+#     html = render_to_string('orders/order/pdf.html',
+#                             {'order': order})
+#     response = HttpResponse(content_type='application/pdf')
+#     response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+#     weasyprint.HTML(string=html).write_pdf(response,
+#         stylesheets=[weasyprint.CSS(
+#             settings.STATIC_ROOT + 'css/pdf.css')])
+#     return response
 
